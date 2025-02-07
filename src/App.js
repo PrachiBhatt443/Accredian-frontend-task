be9456be27944c2da06c258e4045d4c9796f7067
Prachi Bhatt <104725245+PrachiBhatt443@users.noreply.github.com>
2025-02-07 17:30:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
-
+import Navbar from './components/Navbar';
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Hero />
       <Footer/>
     </div>
