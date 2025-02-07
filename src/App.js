04d0b6c434e232a815f89819d0c26b388105517d
Prachi Bhatt <104725245+PrachiBhatt443@users.noreply.github.com>
2025-02-07 17:50:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import Course from './components/Course';
 const App = () => {
   return (
-  <BrowserRouter>
-      
-    <div>
-      <Navbar />
-      {/* <Hero /> */}
-      <Routes>
-          <Route path="/" element={<Hero />} />
-          {/* <Route index element={<Home />} /> */}
-          // <Route path="/courses" element={<Course/>} />
-        {/* </Route> */}
-      </Routes>
+    <>
+      <Navbar/>
+      <Hero />
       <Footer/>
-    </div>
-  </BrowserRouter>
+    </>
   );
 };
 
